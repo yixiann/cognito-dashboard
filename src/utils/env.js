59f8cbce34b479b5fc9019c982029580eec0e3ce
Yi Xian <yixianns@gmail.com>
2025-12-13 13:58:36 +0800

    Migrate to vite

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,8 +1,8 @@
-export const REGION = process.env.REACT_APP_REGION;
-export const AWS_ACCESS_KEY_ID = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
-export const AWS_SECRET_ACCESS_KEY =
-  process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
-export const AWS_USER_POOL_ID = process.env.REACT_APP_AWS_USER_POOL_ID;
+export const REGION = import.meta.env.REACT_APP_REGION;
+export const AWS_ACCESS_KEY_ID = import.meta.env.REACT_APP_AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = import.meta.env
+  .REACT_APP_AWS_SECRET_ACCESS_KEY;
+export const AWS_USER_POOL_ID = import.meta.env.REACT_APP_AWS_USER_POOL_ID;
 
 export const hasNoRegion = REGION === undefined;
 export const hasNoAwsAccessKeyId = AWS_ACCESS_KEY_ID === undefined;
